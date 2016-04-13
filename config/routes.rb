16d46f04096c5804546d9c69b98198d6977e0f56
arakawa <arakawayukio62@gmail.com>
2016-04-13 13:11:58 +0000

    setting root_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  
+  root 'top#index'
 
   resources :blogs
   # The priority is based upon order of creation: first created -> highest priority.
